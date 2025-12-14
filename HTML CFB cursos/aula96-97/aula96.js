62f1a43a9b5afcb028d47c5895628806dcb7cdfe
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-12-14 13:42:47 -0300

    praticando js

--- HTML CFB cursos/aula96-97/aula96.js
+++ HTML CFB cursos/aula96-97/aula96.js
@@ -1,54 +1,32 @@
-const html5 = document.getElementById("html5")
-const css3 = document.getElementById("css3")
-const javaScript = document.getElementById("javaScript")
-const fechar = document.querySelectorAll(".fechar")
-
 const guias = document.querySelectorAll(".guia")
 const conteudos = document.querySelectorAll(".conteudo")
 
-guias.forEach((el) => {
-    el.addEventListener("click", (evt) => {
-        guias.forEach((guia) => {
-            guia.classList.remove("ativo")
-        })
-        conteudos.forEach((conteudo) => {
-            conteudo.classList.remove("visivel")
-            conteudo.classList.add("invisivel")
-        })
-
-
-        const clicado = evt.target
-        if (clicado.innerHTML === "HTML") {
-            clicado.classList.add("ativo")
-
-            html5.classList.remove("invisivel")
-            html5.classList.add("visivel")
-
-        } else if (clicado.innerHTML === "CSS") {
-            clicado.classList.add("ativo")
-
-            css3.classList.remove("invisivel")
-            css3.classList.add("visivel")
-        } else if (clicado.innerHTML === "JavaScript") {
-            clicado.classList.add("ativo")
-
-            javaScript.classList.remove("invisivel")
-            javaScript.classList.add("visivel")
-        }
-
-
+const fecharTudo=()=>{
+    guias.forEach(el=>el.classList.remove("ativo"))
+    conteudos.forEach((el)=>{
+        el.classList.remove("visivel")
+        el.classList.add("invisivel")
     })
+}
+
+document.addEventListener("click",(evt)=>{
+    const fechar=evt.target.closest(".fechar")
+    if(fechar){
+        fecharTudo()
+        return
+    }
+
+    const guia=evt.target.closest(".guia")
+    if(!guia)return
+    fecharTudo()
+
+    guia.classList.add("ativo")
+
+    const alvo=document.getElementById(guia.dataset.target)
+    alvo.classList.remove("invisivel")
+    alvo.classList.add("visivel")
+    
+
 })
 
 
-fechar.forEach((el) => {
-    el.addEventListener("click", () => {
-        guias.forEach((guia) => {
-            guia.classList.remove("ativo")
-        })
-        conteudos.forEach((conteudo) => {
-            conteudo.classList.remove("visivel")
-            conteudo.classList.add("invisivel")
-        })
-    })
-})
