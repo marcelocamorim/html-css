22b32aa2c417926a4f8b53b4862f5238bb3d9c85
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-12-24 18:01:11 -0300

    praticando

---
+++ Curso em video/desafios/desafio10/desafio10.js
@@ -0,0 +1,7 @@
+const burguer=document.getElementById("burguer")
+const containerMenuMobile=document.getElementById("containerMenuMobile")
+
+burguer.addEventListener("click",()=>{
+    containerMenuMobile.classList.toggle("visivel")
+    
+})
