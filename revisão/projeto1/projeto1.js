628a453fd05a950f20a0ce45113cb742b687d7bb
marcelo c. amorim <marceloamorim.dev@gmail.com>
2026-01-07 00:00:25 -0300

    praticando

--- revisão/projeto1/projeto1.js
+++ revisão/projeto1/projeto1.js
@@ -1,6 +1,8 @@
 const parallax = document.querySelector("#paralax");
 const img = parallax.querySelector("img");
 
+const work = document.getElementById("tituloWork")
+
 window.addEventListener("scroll", () => {
   const rect = parallax.getBoundingClientRect();
   const offset = -rect.top * 0.3;
