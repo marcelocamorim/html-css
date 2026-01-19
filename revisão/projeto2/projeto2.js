7114b2fd0f3cc2c6bc35541040004170dd860275
marcelo c. amorim <marceloamorim.dev@gmail.com>
2026-01-19 19:14:47 -0300

    Update projeto2.js

--- revisão/projeto2/projeto2.js
+++ revisão/projeto2/projeto2.js
@@ -1,9 +1,30 @@
-const paraVoceBTN = document.getElementById("para-voce-btn")
-const paraVoceSubmenu =document.getElementById("para-voce-submenu")
+const linksMenu =document.querySelectorAll(".link-menu")
+const sectionSubMenus = document.querySelectorAll(".submenu")
+const setaLinksMenu = document.querySelectorAll(".seta-link-menu")
 
 
-paraVoceBTN.addEventListener("click",()=>{
-    paraVoceSubmenu.classList.toggle("visivel")
-    
+const fecharTudo=()=>{
+    linksMenu.forEach((el=>el.classList.remove("ativo")))
+    sectionSubMenus.forEach((el=>el.classList.remove("ativo")))
+    setaLinksMenu.forEach((el=>el.classList.remove("ativo")))
+}
+
+
+document.addEventListener("click",(evt)=>{
+    const link = evt.target.closest(".link-menu")
+    const seta = evt.target.closest(".seta-link-menu")
+    if(!link)return
+    fecharTudo()
+
+    link.classList.toggle("ativo")
+    seta.classList.toggle("ativo")
+
+    const sectionAlvo = document.getElementById(link.dataset.target)
 })
+
+
+
+
+
+
 
