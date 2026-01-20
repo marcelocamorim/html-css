1eca271c5e16e847276bc4807c943c444e325222
marcelo c. amorim <marceloamorim.dev@gmail.com>
2026-01-19 23:33:04 -0300

    Update projeto2.js

--- revisão/projeto2/projeto2.js
+++ revisão/projeto2/projeto2.js
@@ -1,26 +1,14 @@
-const linksMenu =document.querySelectorAll(".link-menu")
+const linksMenu = document.querySelectorAll(".link-menu")
 const sectionSubMenus = document.querySelectorAll(".submenu")
 const setaLinksMenu = document.querySelectorAll(".seta-link-menu")
 
 
-const fecharTudo=()=>{
-    linksMenu.forEach((el=>el.classList.remove("ativo")))
-    sectionSubMenus.forEach((el=>el.classList.remove("ativo")))
-    setaLinksMenu.forEach((el=>el.classList.remove("ativo")))
+const fecharTudo = () => {
+    sectionSubMenus.forEach((el => el.classList.remove("ativo")))
+    linksMenu.forEach((el => el.classList.remove("ativo")))
+    setaLinksMenu.forEach((el => el.classList.remove("ativo")))
 }
 
-
-document.addEventListener("click",(evt)=>{
-    const link = evt.target.closest(".link-menu")
-    const seta = evt.target.closest(".seta-link-menu")
-    if(!link)return
-    fecharTudo()
-
-    link.classList.toggle("ativo")
-    seta.classList.toggle("ativo")
-
-    const sectionAlvo = document.getElementById(link.dataset.target)
-})
 
 
 
