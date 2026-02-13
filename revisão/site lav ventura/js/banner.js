3f9aecabd5b1555ee711eafe91425db699b0a4c4
marcelo c. amorim <marceloamorim.dev@gmail.com>
2026-02-12 21:20:27 -0300

    Update banner.js

--- revisão/site lav ventura/js/banner.js
+++ revisão/site lav ventura/js/banner.js
@@ -1,3 +1,12 @@
-const slides = document.querySelectorAll(".slides img")
+const slides = document.querySelectorAll(".slides picture")
 let index = 0
 
+function ativarSlide(i){
+    slides.forEach(slide=>slide.classList.remove("active"))
+    slides[i].classList.add("active")
+}
+
+setInterval(()=>{
+    index = (index + 1) % slides.length
+    ativarSlide(index)
+},3000)
