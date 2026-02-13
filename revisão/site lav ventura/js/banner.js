0b4c8aef5d99085937e6219081aa0a094e69e6eb
marcelo c. amorim <marceloamorim.dev@gmail.com>
2026-02-13 11:44:54 -0300

    att banner

--- revisão/site lav ventura/js/banner.js
+++ revisão/site lav ventura/js/banner.js
@@ -1,12 +1,55 @@
+const banner = document.querySelector(".banner")
 const slides = document.querySelectorAll(".slides picture")
-let index = 0
+const btnPrev = document.querySelector(".prev")
+const btnNext = document.querySelector(".next")
 
-function ativarSlide(i){
-    slides.forEach(slide=>slide.classList.remove("active"))
+let index = 0
+let autoSlide = null
+
+function mostrarSlide(i) {
+    slides.forEach(slide => slide.classList.remove("active"))
     slides[i].classList.add("active")
 }
 
-setInterval(()=>{
+
+function startAutoSlide() {
+
+    autoSlide = setInterval(() => {
+        index = (index + 1) % slides.length
+        mostrarSlide(index)
+    }, 1000)
+
+}
+
+function stopAutoSlide(){
+    clearInterval(autoSlide)
+}
+
+startAutoSlide()
+
+banner.addEventListener("mouseenter", stopAutoSlide)
+banner.addEventListener("mouseleave", startAutoSlide)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//buttons prev e next
+btnPrev.addEventListener("click", () => {
+    index = (index - 1 + slides.length) % slides.length
+    mostrarSlide(index)
+})
+
+btnNext.addEventListener("click", () => {
     index = (index + 1) % slides.length
-    ativarSlide(index)
-},3000)
+    mostrarSlide(index)
+})
